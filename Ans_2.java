49db17e3da510b4f271917ed860dc00edbdf2bcf
dhruv1067 <167281182+dhruv1067@users.noreply.github.com>
2025-02-19 17:30:13 +0530

    Add files via upload

---
+++ Ans_2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Ans_2
+{   static int go(int n  , int i){
+    if(i == n + 1)return 0;
+    if(i%2 == 0)return go(n , i + 1) - i;
+    else return go(n , i + 1) + i;
+}
+    public static void main(String[] args) {
+        System.out.println("Enter the number : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(go(n,1));
+    }
+}
+
